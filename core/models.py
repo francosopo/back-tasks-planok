a53d905db05c5b39a99b3c7459c63f99a19e4a77
Fernando Muñoz <fernandomunoz.cl@gmail.com>
2026-04-08 11:47:47 -0400

    add model Task

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    # Definición de opciones para el Estado
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pendiente'
+        COMPLETED = 'completed', 'Completada'
+
+    # Definición de opciones para la Prioridad
+    class Priority(models.TextChoices):
+        LOW = 'low', 'Baja'
+        MEDIUM = 'medium', 'Media'
+        HIGH = 'high', 'Alta'
+        URGENT = 'urgent', 'Urgente'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+    priority = models.CharField(
+        max_length=20,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.get_status_display()})"# Create your models here.
